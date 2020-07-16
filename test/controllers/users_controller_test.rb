649f3969b364581919388318579cb319d111c73a
HarleyNishimura <nishimura.harley@viva-eureka.co.jp>
2020-07-16 15:57:56 +0900

    Add account activation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,10 +15,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch user_path(@other_user), params: {
-                                    user: { password:              FILL_IN,
-                                            password_confirmation: FILL_IN,
-                                            admin: FILL_IN } }
-    assert_not @other_user.FILL_IN.admin?
+                                    user: { password: @other_user.password,
+                                      password_confirmation: @other_user.password,
+                                      admin: true } }
+    assert_not @other_user.reload.admin?
   end
   test "should get new" do
     get signup_path
